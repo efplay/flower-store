14bc175c05162b5dd781146ced5d6b9b0ac4a1aa
Volodymyr <63854674+efplay@users.noreply.github.com>
2025-01-29 16:23:47 +0100

    Changes

--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, UseGuards } from '@nestjs/common'
+import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common'
+import { LoggingInterceptor } from 'src/conception/interceptor'
 import { AuthGuard } from 'src/conception/quard'
 import { FlowersService } from './flowers.service'
 
 @Controller('flowers')
+@UseInterceptors(LoggingInterceptor)
 export class FlowersController {
 	constructor(private readonly flowersService: FlowersService) {}
 
